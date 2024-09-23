ce395f9c34910adc67c3a8b14bbdff4975d62a5e
WDH <pigisduck@163.com>
2024-09-23 15:19:30 +0800

    style(theme-chalk): 调整button的样式

--- docs/demos/button/disabled.vue
+++ docs/demos/button/disabled.vue
@@ -7,6 +7,14 @@
     <sp-button disabled type="warning">警告按钮</sp-button>
     <sp-button disabled type="danger">危险按钮</sp-button>
   </div>
+  <div class="demo-container">
+    <sp-button disabled plain>默认按钮</sp-button>
+    <sp-button disabled plain type="primary">主要按钮</sp-button>
+    <sp-button disabled plain type="success">成功按钮</sp-button>
+    <sp-button disabled plain type="info">信息按钮</sp-button>
+    <sp-button disabled plain type="warning">警告按钮</sp-button>
+    <sp-button disabled plain type="danger">危险按钮</sp-button>
+  </div>
 </template>
 
 <style>
